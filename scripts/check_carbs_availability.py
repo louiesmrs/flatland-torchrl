5bf3be5f3028772f7a24b411d16897c20326e11f
Louie Git <somersl@tcd.ie>
2026-01-31 10:07:23 +0000

    python version

--- scripts/check_carbs_availability.py
+++
@@ -1,7 +0,0 @@
-import pkg_resources
-
-try:
-    dist = pkg_resources.get_distribution('carbs')
-    print('carbs version:', dist.version)
-except pkg_resources.DistributionNotFound:
-    print('carbs not installed')
